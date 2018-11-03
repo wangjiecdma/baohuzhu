3b3891dc61bb5f8eb943dd8c812a86431cae29b8
wangweiwei <wangweiwei@shbst.com>
2018-11-03 17:35:34 +0800

    解决Drawable 下的xml文件错误导致编译出错的问题

---
+++ app/src/main/java/com/szty/baohuzhu/activitys/ActivityPersonalInformation.java
@@ -0,0 +1,21 @@
+package com.szty.baohuzhu.activitys;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.szty.baohuzhu.R;
+
+public class ActivityPersonalInformation extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_information);
+
+    }
+
+}
