03d5930eb50baff28355db20b4240cb255f1eb6e
tengdaiyou <tengdaiyou@Tengdaiyou-MacBP.local>
2018-12-14 22:39:32 +0800

    去掉没有使用的detail2相关的文件

--- app/src/main/java/com/szty/baohuzhu/activitys/ActivityManager.java
+++ app/src/main/java/com/szty/baohuzhu/activitys/ActivityManager.java
@@ -21,7 +21,6 @@ import com.szty.baohuzhu.fragments.FragmentMessageList;
 import com.szty.baohuzhu.fragments.FragmentMyInformation;
 import com.szty.baohuzhu.fragments.FragmentMyProjects;
 import com.szty.baohuzhu.fragments.FragmentProjectDetail;
-import com.szty.baohuzhu.fragments.FragmentProjectDetail2;
 import com.szty.baohuzhu.fragments.FragmentReturnCach;
 import com.szty.baohuzhu.fragments.FragmentSetting;
 
@@ -59,7 +58,7 @@ public class ActivityManager extends BaseActivity  {
         mFragmentMap.put("账户详情",FragmentAccountDetail.class);
 
         mFragmentMap.put("项目详情",FragmentProjectDetail.class);
-        mFragmentMap.put("项目详情2",FragmentProjectDetail2.class);
+//        mFragmentMap.put("项目详情2",FragmentProjectDetail2.class);
 
         //
 
